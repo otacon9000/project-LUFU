cdedeb48fbc7633e657db2f7b1ff91e244ab2895
Andrea Alicino <andrea.alicino@gmail.com>
2023-09-27 14:58:36 -0400

    start Crypt project

---
+++ Source/Crypt/CryptGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CryptGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ACryptGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ACryptGameMode();
+};
+
+
+
---
+++ Source/Crypt/CryptPlayerController.cpp
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+
+#include "CryptPlayerController.h"
+#include "EnhancedInputSubsystems.h"
+
+void ACryptPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// get the enhanced input subsystem
+	if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer()))
+	{
+		// add the mapping context so we get controls
+		Subsystem->AddMappingContext(InputMappingContext, 0);
+
+		UE_LOG(LogTemp, Warning, TEXT("BeginPlay"));
+	}
+}
